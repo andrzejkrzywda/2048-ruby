db8849912dcd4de6b97f019a4ebd6df82c0c398a
Andrzej Krzywda <andrzejkrzywda@gmail.com>
2014-03-25 16:52:00 +0100

    hide @board dependency

--- game.rb
+++ game.rb
@@ -39,12 +39,16 @@ class Game
       (@board.size-1).downto(1) do |col|
         if number_at(row, col) == number_at(row, col-1) && !empty?(col, row) && !added.include?([col, row]) && !added.include?([col-1, row])
           sum = number_at(row, col-1) * 2
-          @board.put_number(sum, row, col)
+          put_number(sum, row, col)
           added << [col, row]
           make_empty(col-1, row)
         end
       end
     end
+  end
+
+  def put_number(sum, row, col)
+    @board.put_number(sum, row, col)
   end
 
   def number_at(row, col)
@@ -56,7 +60,7 @@ class Game
       (0).upto(@board.size-2) do |col|
         if number_at(row, col) == number_at(row, col+1) && !empty?(col, row) && !added.include?([col, row])  && !added.include?([col+1, row])
           sum = number_at(row, col+1) * 2
-          @board.put_number(sum, row, col)
+          put_number(sum, row, col)
           added << [col, row]
           make_empty(col+1, row)
         end
@@ -87,11 +91,11 @@ class Game
   end
 
   def copy_this_to_right(col, row)
-    @board.put_number(number_at(row, col), row, col+1)
+    put_number(number_at(row, col), row, col+1)
   end
 
   def copy_this_to_left(col, row)
-    @board.put_number(number_at(row, col), row, col-1)
+    put_number(number_at(row, col), row, col-1)
   end
 
   def empty?(col, row)
@@ -99,7 +103,7 @@ class Game
   end
 
   def make_empty(col, row)
-    @board.put_number(0, row, col)
+    put_number(0, row, col)
   end
 
 end
